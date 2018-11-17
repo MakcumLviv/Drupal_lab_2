1b4def7886b5d25939d05c9a5e65baa3a3d15693
Max Yezerskyi <m.yezerskyi@gmail.com>
2018-11-17 18:42:30 +0200

    small changes

--- js/script.js
+++ js/script.js
@@ -3,7 +3,11 @@ const COUNTRIES = [];
 
 document.getElementById('form').addEventListener('submit', (e) => {
     let name = document.getElementById("name").value;
-    createСountry(name);
+    if (name == '') {
+        alert("No Data");
+    } else {
+        createСountry(name);
+    }
     e.preventDefault();
 });
 
@@ -13,57 +17,42 @@ function quantityCountries() {
     count.innerHTML = message;
 }
 
-function createСountry(name) {
-    let сountry = { name }
-    COUNTRIES.push(сountry);
-    readСountry();
-    quantityCountries();
-    document.getElementById('form').reset();
-    console.log(COUNTRIES);
-}
-
 function readСountry() {
-    let сountryhtml = document.getElementById('сountry');
+    let сountryhtml = document.getElementById('countries');
     сountryhtml.innerHTML = "";
     for (let i = 0; i < COUNTRIES.length; i++) {
         сountryhtml.innerHTML += `
-		<div class="black">
-
-		<button class="edit" onClick="editCountry('${i}')"> Edit </button>
-		<button class="remove" onClick="remove('${i}')"> Delete </button>
-		<span> ${COUNTRIES[i].name} </span>
-		</div> `
+        <tr class="black">
+        <td> ${COUNTRIES[i].name}  </td>
+        <td><button class="edit" onClick="editCountry('${i}')"> Edit </button></td>
+        <td><button class="remove" onClick="remove('${i}')"> Delete </button></td>
+        </tr> `
     }
 }
 
-function remove(i) {
-    COUNTRIES.splice(i, 1);
-    readСountry();
-    quantityCountries();
-
-}
 
 function editCountry(index) {
-    let сountryhtml = document.getElementById('сountry');
+    let сountryhtml = document.getElementById('countries');
     сountryhtml.innerHTML = "";
     for (let i = 0; i < COUNTRIES.length; i++) {
         if (i == index) {
-            сountryhtml.innerHTML += ` <div class="red">
-			<input id="input2name" placeholder="${COUNTRIES[i].name}"> 
-			<button class="edit" onClick="updateCountry('${i}')">Update</button>
-			<button class="remove" onClick="readСountry()">Cancel</button>
-			</div>  `
+            сountryhtml.innerHTML += `
+            <tr class="red">
+            <td><input id="input2name" type="text" placeholder="${COUNTRIES[i].name}"> </td>
+            <td><button class="edit" onClick="updateCountry('${i}')">Update</button></td>
+            <td><button class="remove" onClick="readСountry()">Cancel</button></td>
+            </tr> `
         } else {
-            сountryhtml.innerHTML += ` <div class="black">
-			<span> ${COUNTRIES[i].name} </span>
-			<button disabled class="edit" onClick="editCountry('${i}')">Edit</button>
-			<button disabled class="remove" onClick="remove('${i}')">Delete</button>
-
-			</div>  `
-
+            сountryhtml.innerHTML += `
+            <tr class="black">
+            <td> ${COUNTRIES[i].name} </td>
+            <td><button disabled class="edit" onClick="editCountry('${i}')"> Edit </button></td>
+            <td><button disabled class="remove" onClick="remove('${i}')"> Delete </button></td>
+            </tr>  `
         }
     }
 }
+
 
 function updateCountry(index) {
     let updatename = document.getElementById('input2name').value;
@@ -73,4 +62,18 @@ function updateCountry(index) {
         COUNTRIES[index].name = updatename;
         readСountry();
     }
+}
+
+function remove(i) {
+    COUNTRIES.splice(i, 1);
+    readСountry();
+    quantityCountries();
+}
+
+function createСountry(name) {
+    let сountry = { name }
+    COUNTRIES.push(сountry);
+    readСountry();
+    quantityCountries();
+    document.getElementById('form').reset();
 }
