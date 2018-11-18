2dda41f34e980592e566aa8f9c4c23646b77e4fe
Max Yezerskyi <m.yezerskyi@gmail.com>
2018-11-18 19:20:29 +0200

    small changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const COUNTRIES = [];
 
-
 document.getElementById('form').addEventListener('submit', (e) => {
     let name = document.getElementById("name").value;
     if (name == '') {
@@ -21,8 +20,7 @@ function readСountry() {
     let сountryhtml = document.getElementById('countries');
     сountryhtml.innerHTML = "";
     for (let i = 0; i < COUNTRIES.length; i++) {
-        сountryhtml.innerHTML += `
-        <tr class="black">
+        сountryhtml.innerHTML += `<tr class="black">
         <td> ${COUNTRIES[i].name}  </td>
         <td><button class="edit" onClick="editCountry('${i}')"> Edit </button></td>
         <td><button class="remove" onClick="remove('${i}')"> Delete </button></td>
@@ -30,21 +28,18 @@ function readСountry() {
     }
 }
 
-
 function editCountry(index) {
     let сountryhtml = document.getElementById('countries');
     сountryhtml.innerHTML = "";
     for (let i = 0; i < COUNTRIES.length; i++) {
         if (i == index) {
-            сountryhtml.innerHTML += `
-            <tr class="red">
+            сountryhtml.innerHTML += `<tr class="red">
             <td><input id="input2name" type="text" placeholder="${COUNTRIES[i].name}"> </td>
             <td><button class="edit" onClick="updateCountry('${i}')">Update</button></td>
             <td><button class="remove" onClick="readСountry()">Cancel</button></td>
             </tr> `
         } else {
-            сountryhtml.innerHTML += `
-            <tr class="black">
+            сountryhtml.innerHTML += `<tr class="black">
             <td> ${COUNTRIES[i].name} </td>
             <td><button disabled class="edit" onClick="editCountry('${i}')"> Edit </button></td>
             <td><button disabled class="remove" onClick="remove('${i}')"> Delete </button></td>
@@ -52,7 +47,6 @@ function editCountry(index) {
         }
     }
 }
-
 
 function updateCountry(index) {
     let updatename = document.getElementById('input2name').value;
